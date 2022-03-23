47b25e16840cb452d57453432c91eaccdab6c068
DenizhanSarac <78859953+DenizhanSarac@users.noreply.github.com>
2022-03-23 20:31:59 +0300

    Create corona_virous_page.dart

---
+++ lib/features/presentation/pages/corona_virous_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+//import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class CoronaVirusPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: "https://covid.cdc.gov/",
+    );
+  }
+}
